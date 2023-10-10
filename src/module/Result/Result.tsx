d0c38ceb1078ea4fb0ad27c777b37cd9400691c8
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-10-10 14:11:01 +0200

    Result.tsx, Result.module.scss edited

--- src/module/Result/Result.tsx
+++ src/module/Result/Result.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import style from "./Result.module.scss"
 
 type Props = {}
 
 export const Result = (props: Props) => {
-  return <div>Result</div>
+  return <div className={style.result}>Result</div>
 }
--- src/module/Search/Search.tsx
+++ src/module/Search/Search.tsx
@@ -1,15 +1,28 @@
 import styles from "./Search.module.scss"
 import { ReactComponent as SearchIcon } from "./search.svg"
+import { useState } from "react"
 type Props = {}
 
 export const Search = (props: Props) => {
+  const [search, setSearch] = useState("")
   const handlerSubmit = (e: React.FormEvent<EventTarget>) => {
     e.preventDefault()
-    console.log(e)
   }
+
+  const handleChange = (e: React.ChangeEvent<EventTarget>) => {
+    if (e.target instanceof HTMLInputElement) {
+      setSearch(e.target.value)
+    }
+  }
+
   return (
     <form className={styles.form} onSubmit={handlerSubmit}>
-      <input type='search' className={styles.search} />
+      <input
+        type='search'
+        className={styles.search}
+        onChange={handleChange}
+        value={search}
+      />
       <button className={styles.btn} type='submit'>
         <SearchIcon />
       </button>
