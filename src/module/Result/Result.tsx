3284c35dff48a5f821b17e00508b4315f18ea657
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-10-10 14:16:33 +0200

    npm i @reduxjs/toolkit react-redux @types/react-redux

--- src/module/Result/Result.tsx
+++ src/module/Result/Result.tsx
@@ -4,5 +4,10 @@ import style from "./Result.module.scss"
 type Props = {}
 
 export const Result = (props: Props) => {
-  return <div className={style.result}>Result</div>
+  return (
+    <div className={style.result}>
+      <p>25&#8451;</p>
+      <p>Kaliningrad</p>
+    </div>
+  )
 }
